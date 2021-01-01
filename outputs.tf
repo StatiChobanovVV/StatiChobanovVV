9108b7d5d34fc2dd3d1a9124ce4c756752736d53
Alexander Nikolov <alexandre.nikolov@iress.com>
2021-01-01 09:58:32 +0200

    sqs and db otputs added

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "vpc" {
   value = data.terraform_remote_state.state.outputs.vpc
   description = "API Stage resources"
 }
+
+output "sqs" {
+  value       = data.terraform_remote_state.state.outputs.sqs
+  description = "Queues for exchanging messages between VVIT and IUK"
+}
+
+output "db" {
+  value = data.terraform_remote_state.state.outputs.db
+  description = "The outputs from the DB module"
+}
