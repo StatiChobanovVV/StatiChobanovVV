217040147615851655b72c97091018d7cde4e6e2
Aleksandar Nikolov <alexander.nikolov@inetbfa.com>
2021-04-06 20:35:11 +0300

    support for different environments

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+locals {
+  env_suffix = var.environment!="production" ? "-${var.environment}":""
+}
+
 data "terraform_remote_state" "state" {
   backend = "s3"
   config = {
     encrypt = "true"
-    bucket = "base-iuk-terraform-bucket"
-    key  = "${var.environment}-base-infra/shared.tfstate"
+    bucket = "base-iuk-terraform-bucket${local.env_suffix}"
+    key  = "dev-base-infra/shared.tfstate"
     region = var.region
   }
 }
